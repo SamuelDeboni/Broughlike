d9c31c5dd80ef6b1e42697bc467be7d63c187373
SamuelDeboni <samueldeboniss@gmail.com>
2019-06-02 13:27:06 -0300

    Unity movment is done

--- Scripts/Units.cs
+++ Scripts/Units.cs
@@ -4,28 +4,54 @@ using System.Collections.Generic;
 
 public class Units : Node2D
 {
-    [Export]
-    public KinematicBody2D[] playerUnits;
-    int selectedUnity = 0;
+    public Unity[] playerUnits;
+    public int selectedUnity = 0;
+    int unityCount = 0;
+    public int turnCount;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        playerUnits = new KinematicBody2D[GetChildCount()];
+        /*  get the amount of units,
+            the units need to have index 0 
+            to unity count */
+        foreach(var c in GetChildren())
+            if(c is Unity) unityCount++;
+
+        playerUnits = new Unity[unityCount];
 
         for(int i = 0; i < playerUnits.Length; i++)
-            playerUnits[i] = GetChild(i) as KinematicBody2D;
-        
-        foreach(var u in playerUnits)
-        {
-            if(u != null)
-                GD.Print(u);
-        }
+            playerUnits[i] = GetChild(i) as Unity;
+
+        GD.Print("unityCount = " + unityCount);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        // Units movement
+        if(selectedUnity < unityCount)
+        {
+            if(Input.IsActionJustPressed("move_right"))
+                playerUnits[selectedUnity].moveUnity(0);
+            else if(Input.IsActionJustPressed("move_up"))
+                playerUnits[selectedUnity].moveUnity(1);
+            else if(Input.IsActionJustPressed("move_left"))
+                playerUnits[selectedUnity].moveUnity(2);
+            else if(Input.IsActionJustPressed("move_down"))
+                playerUnits[selectedUnity].moveUnity(3);
+        }
+        else
+            nextTurn();
+
+    }
+
+    void nextTurn()
+    {
+        selectedUnity = 0;
+        foreach(var u in playerUnits)
+        {
+            u.moves = 1;
+        }
+    }
 }
--- Scripts/Unity.cs
+++ Scripts/Unity.cs
@@ -3,19 +3,77 @@ using System;
 
 public class Unity : KinematicBody2D
 {
-
     Units units;
+    Vector2 targetPos;
+
+
+    public int moves = 1;
 
     public override void _Ready()
     {
         units = GetNode("..") as Units;
-
-
+        targetPos = Position; 
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Process(float delta)
+    {
+        // move the character
+
+        if((Position-targetPos).Length() > 5)
+            Position += (targetPos-Position)*0.5f;
+        else
+            Position = targetPos;       
+    }
+
+
+    public void moveUnity(int dir)
+    {
+        if(moves > 0)
+        {
+            switch (dir)
+            {
+                case 0:
+                    RotationDegrees = 0;
+                    if(!CollisionTest(targetPos + new Vector2(32,0)))
+                        targetPos.x += 32;
+                break;
+
+                case 1:
+                    RotationDegrees = 270;
+                    if(!CollisionTest(targetPos - new Vector2(0,32)))
+                        targetPos.y -= 32;
+                break;
+
+                case 2:
+                    RotationDegrees = 180;
+                    if(!CollisionTest(targetPos - new Vector2(32,0)))
+                        targetPos.x -= 32;
+                break;
+
+                case 3:
+                    RotationDegrees = 90;
+                    if(!CollisionTest(targetPos + new Vector2(0,32)))
+                        targetPos.y += 32;
+                break;
+            }
+            moves--;
+        }
+        
+        if(moves == 0)
+        {
+            units.selectedUnity++;
+        }
+    }
+
+    /* Test if the unity will colide */
+    bool CollisionTest(Vector2 pos)
+    {
+        foreach(var u in units.playerUnits)
+        {
+            var uk = u as KinematicBody2D;
+            if(uk != null && uk.Position == pos)
+                return true;
+        }
+        return false;
+    }
 }
